05c3dea96d9f117f0711e4ceccaf3b3c8c02f565
SpeekeR <DominikZappe1999@seznam.cz>
2024-05-01 21:41:55 +0200

    Final version of Main + requirements.txt for Python part introduced

--- src/cpp_indexer/Main.cpp
+++ src/cpp_indexer/Main.cpp
@@ -37,6 +37,20 @@ void parse_args(int argc, char **argv) {
 }
 
 /**
+ * Create necessary directories if they do not exist
+ */
+void mkdirs() {
+    if (!std::filesystem::exists("../data"))
+        std::filesystem::create_directory("../data");
+    if (!std::filesystem::exists("../index"))
+        std::filesystem::create_directory("../index");
+    if (!std::filesystem::exists("../index_file_based"))
+        std::filesystem::create_directory("../index_file_based");
+    if (!std::filesystem::exists("../download"))
+        std::filesystem::create_directory("../download");
+}
+
+/**
  * Main function
  * @return Exit code
  */
@@ -44,101 +58,12 @@ int main(int argc, char **argv) {
     /* Parse arguments, set FILE_BASED and DETECT_LANG */
     parse_args(argc, argv);
 
+    /* Create directories if they do not exist */
+    mkdirs();
+
     /* Run the GUI */
     GUI gui = GUI();
     gui.run();
-
-//    auto docs = IndexHandler::load_documents("../data");
-//    auto [tokenized_docs, positions_map] = IndexHandler::preprocess_documents(docs);
-//    auto indexer = Indexer(docs, tokenized_docs, positions_map);
-//
-//    IndexHandler::save_index(indexer, "../index/index1.json");
-//    auto indexer = Indexer();
-//    IndexHandler::load_index(indexer, "../index/index1.json");
-//
-//    std::string query = "Geralt of Rivia";
-//    auto [docs_result, scores, positions] = IndexHandler::search(indexer, query, 3, FieldType::ALL);
-//    auto positions_geralt = positions["geralt"][954];
-//    for (const auto &pos : positions_geralt)
-//        std::cout << pos << " ";
-//    std::cout << std::endl;
-//
-//    /* Open Magistrovo hlaseni.json */
-//    std::string doc_path = "../data/Magistrovo hlaseni.json";
-//    auto doc = DataLoader::load_json_document(doc_path);
-//    Preprocessor preprocessor = Preprocessor();
-//    auto [tokens, _] = preprocessor.tokenize(doc.content);
-//
-//    std::cout << tokens[237] << std::endl;
-//
-//    /* Run the GUI */
-//    GUI gui = GUI();
-//    gui.run();
-
-//    std::string base_dir = FILE_BASED_INDEX_PATH + "index1/";
-//    if (!std::filesystem::exists(base_dir))
-//        std::filesystem::create_directory(base_dir);
-//
-//    /* Load documents */
-//    {
-//        auto docs = IndexHandler::load_documents("../data");
-//        FileBasedLoader::save_doc_cache(docs, base_dir);
-//    }
-//
-//    /* Preprocess documents */
-//    {
-//        std::vector<Document> docs;
-//        {
-//            auto doc_cache = FileBasedLoader::load_doc_cache(base_dir);
-//            for (const auto &[id, doc]: doc_cache)
-//                docs.push_back(doc);
-//        }
-//        auto [tokenized_docs, positions_map] = IndexHandler::preprocess_documents(docs);
-//        FileBasedLoader::save_tokenized_docs(tokenized_docs, base_dir);
-//        FileBasedLoader::save_positions_map(positions_map, base_dir);
-//    }
-//
-//    /* Index documents */
-//    auto indexer = Indexer(base_dir);
-//
-//    /* Save the index */
-//    IndexHandler::save_index(indexer, "../index/index1.json");
-//
-//    /* Load the index */
-//    indexer = Indexer();
-//    IndexHandler::load_index(indexer, "../index/index1.json");
-//
-//    /* Search "Geralt z Rivie" */
-//    std::string query = "Geralt Rivie";
-//    auto [docs_result, scores, positions] = IndexHandler::search(indexer, query, 3, FieldType::ALL, 3);
-//    auto [snippet, highlight_index] = IndexHandler::create_snippet(indexer, docs_result[0].id, positions, 30);
-//    std::cout << snippet << std::endl;
-//    for (const auto &index : highlight_index)
-//        std::cout << index << " ";
-//    std::cout << std::endl << std::endl;
-//    std::tie(docs_result, scores, positions) = IndexHandler::search(indexer, query, 3, FieldType::TITLE);
-//
-//    /* Update the document, so it contains "Geralt z Rivie" more and so it is way more relevant (see Score print) */
-//    std::vector<int> new_doc_ids = {550};
-//    std::vector<Document> new_docs = {{550, {"už ne Geralt z Rivie"}, {}, {}, {}, {}, {"Geralt z Rivie Geralt z Rivie Geralt z Rivie Geralt z Rivie"}}};
-//    IndexHandler::update_docs(indexer, new_doc_ids, new_docs);
-//
-//    /* Search "Geralt z Rivie" again */
-//    result = IndexHandler::search(indexer, query, 3);
-//
-//    /* Remove the document completely now and see how the score of others changes too, because overall IDF changes */
-//    IndexHandler::remove_docs(indexer, new_doc_ids);
-//
-//    /* Search "Geralt z Rivie" again and see how the score of others changes (also ID 550 is gone, shocking!) */
-//    result = IndexHandler::search(indexer, query, 3);
-//
-//    /* Search "Geralt z Rivie" boolean */
-//    query = "NOT Geralt AND (z OR NOT NOT Rivie)";
-//    auto result_bool = IndexHandler::search(indexer, query);
-//    result_bool = IndexHandler::search(indexer, query, FieldType::TITLE);
-//
-//    /* Download https://zaklinac.fandom.com/wiki/Geralt_z_Rivie */
-//    IndexHandler::add_doc_url(indexer, "https://zaklinac.fandom.com/wiki/Geralt_z_Rivie");
 
     return EXIT_SUCCESS;
 }
